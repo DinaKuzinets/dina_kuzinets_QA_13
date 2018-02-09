fd8895bd20684452e95ac7cf47491d2d2b82f155
DinaKuzinets <dkuzinets@gmail.com>
2018-02-09 11:54:14 +0200

    TrelloLogin_extract_method

---
+++ TrelloTest/src/main/java/LoginTrelloTest.java
@@ -0,0 +1,67 @@
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+public class LoginTrelloTest {
+    FirefoxDriver wd;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void LoginTrelloTest() {
+        openSite();
+       // wd.findElement(By.xpath("//div[2]")).click();
+        clickLoginButton();
+        enterUserName("elena.telran@yahoo.com");
+        enterPassword("12345.com");
+        confirmLogin();
+    }
+
+    private void confirmLogin() {
+        wd.findElement(By.id("login")).click();
+    }
+
+    private void enterPassword(String password) {
+        wd.findElement(By.id("password")).click();
+        wd.findElement(By.id("password")).clear();
+        wd.findElement(By.id("password")).sendKeys(password);
+    }
+
+    private void enterUserName(String userName) {
+        wd.findElement(By.id("user")).click();
+        wd.findElement(By.id("user")).clear();
+        wd.findElement(By.id("user")).sendKeys(userName);
+    }
+
+    private void openSite() {
+        wd.get("https://trello.com/");
+    }
+
+    private void clickLoginButton() {
+        wd.findElement(By.linkText("Log In")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
