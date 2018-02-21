0e8a489f17f23c82db6e9a588b802ac22ea40462
DinaKuzinets <dkuzinets@gmail.com>
2018-02-21 22:34:58 +0200

    BoardTestsPublicMethods

--- TrelloTest/src/test/java/com/telRan/course/DeleteBoardTest.java
+++ TrelloTest/src/test/java/com/telRan/course/DeleteBoardTest.java
@@ -3,11 +3,11 @@ package com.telRan.course;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-public class DeleteBoard extends TestBase {
+public class DeleteBoardTest extends TestBase {
 
 
 
-    private String boardName = "TestBoard_1";
+    public String boardName = "TestBoard";
 
     @Test
     public void DelBoard() {
@@ -25,24 +25,24 @@ public class DeleteBoard extends TestBase {
         //   wd.findElement(By.cssSelector("div.chrome.chrome-64.windows.body-webkit-scrollbars.body-tabbed-page:nth-child(2) div.member-boards-view div.js-boards-page div.js-react-root div.boards-page-board-section.mod-no-sidebar:nth-child(1) ul.boards-page-board-section-list li.boards-page-board-section-list-item:nth-child(5) a.board-tile span.board-tile-details.is-badged > span.board-tile-details-name")).click();
     }
 
-    private void openMenuMore() {
+    public void openMenuMore() {
         wd.findElement(By.cssSelector("span.icon-sm.icon-overflow-menu-horizontal.board-menu-navigation-item-link-icon")).click();
     }
 
-    private void selectCloseBoard() {
+    public void selectCloseBoard() {
         wd.findElement(By.cssSelector("a.board-menu-navigation-item-link.js-close-board")).click();
     }
 
-    private void clickConfirmCloseButton() {
+    public void clickConfirmCloseButton() {
         wd.findElement(By.cssSelector("input.js-confirm.full.negate")).click();
 
     }
 
-    private void returnToHomepage() {
+    public void returnToHomepage() {
         wd.findElement(By.cssSelector("span.header-btn-icon.icon-lg.icon-board-back-to-home.light")).click();
     }
 
-    private void clickSpareSpace() {
+    public void clickSpareSpace() {
         wd.findElement(By.cssSelector("div.boards-page-board-section-header")).click();
     }
 }
